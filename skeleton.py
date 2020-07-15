4aa8653d18a3d78e047aee8cf45a977e7f9fbc31
Henry Liu <hliu117@aucklanduni.ac.nz>
2020-07-15 21:55:16 +1200

    updated skeleton

---
+++ skeleton.py
@@ -0,0 +1,20 @@
+# Name of the challenge goes here as well as any inputs required and potential return type
+def challenge_name():
+    pass
+
+if __name__ == "__main__":
+    pass
+    # Check for test cases 
+    inputs1 = []
+    output1 = challenge_name()
+    answer1 = []
+    if answer1 == output1:
+        print("Test Case 1 is correct")
+
+    # Do this maybe like 2-3 times 
+
+    # If everything is correct then 
+    print("Task Completed")
+
+    # Else: 
+    print("Incorrect")
