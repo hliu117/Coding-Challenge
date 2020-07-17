10761635e79fd56b9d1b13e72624c5f1a280986a
Joshua Holroyd <jhol491@aucklanduni.ac.nz>
2020-07-17 16:00:48 +1200

    minor display adjustment

--- skeleton.py
+++ skeleton.py
@@ -16,15 +16,16 @@ if __name__ == "__main__":
             outputs.append(None)
 
     completed = True
+    print("\n")
     for i, out in enumerate(outputs):
         if out == answers[i]:
             print("Test Case", i+1, "is correct")
         else:
-            print("Test Case", i+1, "is incorrect")
+            print("Test Case", i+1, "is incorrect..")
             print("Expected:", answers[i])
             print("Your Output:", out)
             completed = False
-            print("")
+        print("\n")
 
     if completed:
         print("Task Completed")
