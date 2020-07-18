ec0a3f28b6e005259c1e071adc128dc5ef4a0461
Henry Liu <hliu117@aucklanduni.ac.nz>
2020-07-18 20:34:59 +1200

    Create scrambled_challenge.py

---
+++ scrambled_challenge.py
@@ -0,0 +1,33 @@
+# Name of the challenge goes here as well as any inputs required and potential return type
+def challenge_name(x,y):
+    return x, y
+
+if __name__ == "__main__":
+    # Check for test cases 
+    inputs = [(1,2),(5,6)]
+    answers = [(1,2),(5,6)]
+
+    outputs = []
+    for inp in inputs:
+        try:
+            outputs.append(challenge_name(*inp))
+        except Exception as e:
+            print("ERROR:",e)
+            outputs.append(None)
+
+    completed = True
+    print("\n")
+    for i, out in enumerate(outputs):
+        if out == answers[i]:
+            print("Test Case", i+1, "is correct")
+        else:
+            print("Test Case", i+1, "is incorrect..")
+            print("Expected:", answers[i])
+            print("Your Output:", out)
+            completed = False
+        print("\n")
+
+    if completed:
+        print("Task Completed")
+    else: 
+        print("Task Not Complete")
