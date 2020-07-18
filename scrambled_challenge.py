3697ae2ccaa421257fa61eae16e8aa7580692944
Henry Liu <hliu117@aucklanduni.ac.nz>
2020-07-18 21:08:16 +1200

    Update scrambled_challenge.py

--- scrambled_challenge.py
+++ scrambled_challenge.py
@@ -1,16 +1,27 @@
-# Name of the challenge goes here as well as any inputs required and potential return type
-def challenge_name(x,y):
-    return x, y
+# The Scrambled challenge:
+# Given two words a and b return True if b is a perfect scramble of the word a. 
+# A perfect scramble means that both words have to be the same length and all letters of the 
+# word b are used in the word a
+
+# Complete the function scrambled
+# Inputs:
+#   - a, a word
+#   - b, a word that may or may not be a perfect scramble of word a
+
+# Outputs in the order specified
+#   - A boolean True or false, depending whether or not the word b is a scrambled version of the word a
+def scrambled(a,b):
+    pass
 
 if __name__ == "__main__":
     # Check for test cases 
-    inputs = [(1,2),(5,6)]
-    answers = [(1,2),(5,6)]
+    inputs = [("great","greater"),("josh","hsoj"),("henry","yenry")]
+    answers = [False,True,False]
 
     outputs = []
     for inp in inputs:
         try:
-            outputs.append(challenge_name(*inp))
+            outputs.append(scrambled(*inp))
         except Exception as e:
             print("ERROR:",e)
             outputs.append(None)
@@ -25,7 +36,7 @@ if __name__ == "__main__":
             print("Expected:", answers[i])
             print("Your Output:", out)
             completed = False
-        print("\n")
+        print("")
 
     if completed:
         print("Task Completed")
