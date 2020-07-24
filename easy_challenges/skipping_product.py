936c41238c915d0cab7089f8fbca9f8bd66db4cb
Henry Liu <hliu117@aucklanduni.ac.nz>
2020-07-24 17:29:52 +1200

    Update skipping_product.py

--- easy_challenges/skipping_product.py
+++ easy_challenges/skipping_product.py
@@ -1,15 +1,15 @@
 # The Skipping Product Challenge
-# Given an list of ints, return a list of the products of all integers in the list excluding the current in the calculation of that product.
+# Given an list of ints, return a list of the products of all integers in the list excluding the current index in the calculation of that product.
 
 # Example:
-# A = ["hello","my","name","is","bob"]
-# Return 5 cause hello is the longest word and it is of length 5
+# A = [1,2,3,4]
+# Return [24,12,8,6]
 
 # Inputs
-#   - A List of words (Strings) 
+#   - A List of ints 
 
 # Outputs
-#   - An integer representing the length of the longest word
+#   - A List of ints 
 def skipping_product(A):
     # Write your code here:
 
@@ -18,8 +18,8 @@ def skipping_product(A):
 if __name__ == "__main__":
     print("Skipping Product Challenge")
     # Check for test cases 
-    inputs = [[1,2,3,4,5,6,7,8,9,10]]
-    answers = [[3628800.0, 1814400.0, 1209600.0, 907200.0, 725760.0, 604800.0, 518400.0, 453600.0, 403200.0, 362880.0]]
+    inputs = [[1,2,3,4],[1,2,3,4,5,6,7,8,9,10]]
+    answers = [[24,12,8,6],[3628800.0, 1814400.0, 1209600.0, 907200.0, 725760.0, 604800.0, 518400.0, 453600.0, 403200.0, 362880.0]]
 
     outputs = []
     for inp in inputs:
